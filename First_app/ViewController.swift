95b95e1645910a13df9cd0b4963007090c030bbf
Liguo Jiao's Mac <jiaoliguo888@gmail.com>
2015-02-15 04:11:29 +1300

    d

--- First_app/ViewController.swift
+++ First_app/ViewController.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    /*
+    Global variable
+    */
+    @IBOutlet var Disaplay_label: UILabel!
 
+    @IBOutlet var Input_field: UITextField!
+    
+    @IBAction func Input_enter(sender: UITextField) {
+        Disaplay_label.text = sender.text
+        sender.text = nil
+    }
+    
+    @IBAction func Enter(sender: UIButton) {
+        Disaplay_label.text = Input_field.text
+        Input_field.text = nil
+    }
+    @IBAction func Clean_B(sender: UIButton) {
+        Input_field.text = nil
+    }
+    
+    /*functions*/
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +39,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
