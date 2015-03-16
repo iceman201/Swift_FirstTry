b6f62180601f9302570d24fc17ea2fa8f0fd7e13
Liguo Jiao's Mac <jiaoliguo888@gmail.com>
2015-03-17 03:42:31 +1300

    Finished who I am

--- First_app/ViewController.swift
+++ First_app/ViewController.swift
@@ -16,15 +16,19 @@ class ViewController: UIViewController {
     var bar_backGround = UIColor(red: 239/255, green: 49/255, blue: 37/255, alpha: 0)
     /*functions*/
     
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.navigationBarHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.tintColor = bar_color
         self.navigationController?.navigationBar.barTintColor = bar_backGround
         self.navigationController?.navigationBar.tintColor = bar_color
-        
+        self.navigationController?.navigationBarHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- First_app/WhoIam.swift
+++ First_app/WhoIam.swift
@@ -10,27 +10,25 @@ import UIKit
 
 class WhoIam: UIViewController {
 
+    @IBOutlet var textView: UITextView!
+    let content = "Hi there \n\nI was born in Beijing, China move to Christchurch, New Zealand when I was 18 years old. I received my first computer in 1996, when I was 6 years old. It was running Windows 95. I loved it withou any reason, I felt like the world was at my fingertips, I could do anything. \n\nI wasn't sure about what I'm gona do rest of my life at that time, until that when I was in junior high school, a day I was asking my physics teacher some questions about exam, meanwhile I heared from him about his computer system was got virus, and he was really need it to be fixed. So I was just help him recover his computer system. After finished, we had a conversation about where I got know about computer and why I like these computer stuff, he said “have you ever thought about what you gona do in your future? if not I think you should focus more about computer stuff because it's the thing you love from deep of your mine and you are enjoy and willing to do it without any reasons which is call 'interest', if you got a thing which you deeply interested just stick with it try to turn it to your strength, the thing I said you may not understand, just remember it.“ that was quite influenced my life, because I reliaze how much I love computer. \n\nTherefore, I started learning C and C++ by myself since high school. After graduated from high school, during the summer I started learning Python. If say my physics teacher enlighten about how much I love Computer, then High school is the time I realize how much I love programming. Each time I made a mistake on coding I would spend hours figuring out a solution, determined to not let the bug get the better of me. I would feel upset until I solved this bug, and when I would be overcome with a great sense of accomplishment."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textView.text = content //give a content to textview
+        textView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0) //setup content begin
+        textView.font = UIFont.systemFontOfSize(18) //content font size
+        view.addSubview(textView) //setup the textview
+        textView.textColor = UIColor.whiteColor() //setup content fontcolor
+        
         self.navigationController?.navigationBarHidden = false
         
-        // Do any additional setup after loading the view.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
